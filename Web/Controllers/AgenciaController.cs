5a35bc18722de0fb962815254ea44b1e10534941
Fernando casagrande <gerencia.desenv@furacao.com.br>
2023-09-05 17:08:19 -0300

    Inicio do Projeto WEB

---
+++ Web/Controllers/AgenciaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AgenciaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/Controllers/BancoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class BancoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
