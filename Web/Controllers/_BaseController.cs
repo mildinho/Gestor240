54cd908e79433d68c389f9cd306ee6f9b0ccf454
Fernando casagrande <gerencia.desenv@furacao.com.br>
2023-10-30 14:40:04 -0300

    Criação do Base Controller

---
+++ Web/Controllers/_BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public abstract class _BaseController<T> : Controller where T : _BaseController<T>
+    {
+
+        [Route("/PageNotFound")]
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
