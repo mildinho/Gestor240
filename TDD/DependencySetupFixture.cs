5e9eacfa1998518a040e9d99dd083e745f308413
Fernando casagrande <gerencia.desenv@furacao.com.br>
2023-11-22 18:25:50 -0300

    Configuracao do TDD

--- TDD/DependencySetupFixture.cs
+++ TDD/DependencySetupFixture.cs
@@ -2,25 +2,39 @@
 using Infra.Data.Contexto;
 using Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TDD
 {
     public class DependencySetupFixture 
     {
-
+        //https://stackoverflow.com/questions/55497800/populate-iconfiguration-for-unit-tests
         private string ConexaoTest = $"Server = localhost; userid = developer; password = furacao123adm; database = _Gesto240_Teste";
+        private string ConfiguracaoDB = "MYSQL";
+
 
         public ServiceProvider ServiceProvider { get; private set; }
-
+        public IConfiguration configuration { get; private set; }
 
         public DependencySetupFixture()
         {
             var serviceCollection = new ServiceCollection();
+            //configuration = new ConfigurationBuilder();
+
+            
             ServerVersion serverVersion;
 
-            serverVersion = new MySqlServerVersion(
-                ServerVersion.AutoDetect(ConexaoTest));
+            ConfiguracaoDB = configuration.GetConnectionString("BancoDados").ToUpper();
+            if (ConfiguracaoDB == "MYSQL")
+            {
+                serverVersion = new MySqlServerVersion(ServerVersion.AutoDetect(ConexaoTest));
+            }
+            else
+            {
+                serverVersion = new MariaDbServerVersion(ServerVersion.AutoDetect(ConexaoTest));
+            }
+
 
             serviceCollection.AddDbContext<DBContexto>(options =>
             {
@@ -30,6 +44,8 @@ namespace TDD
             });
 
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
 
             ServiceProvider = serviceCollection.BuildServiceProvider();
 
